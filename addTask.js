463d0943199fe839200ff27f1aa2dfbb33a7d1dd
Heba <habeelkenzy@gmail.com>
2019-02-26 18:48:10 +0200

    styling tasks list

--- addTask.js
+++ addTask.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
 	}
 		$('#add').click(function write() {
 			var newtask = $('#newtask').val();
-
+			
 			$('#list').append('<li><input class="complete"  id="item" type="checkbox"/>' + newtask + '<span aria-hidden="true"  class="deletetask">&times;</span></li>');
 			app.tasks.push(newtask);
 			localStorage.setItem('app', JSON.stringify(app));
